4ec2d30018f86e29c8d684b27e32a01fe8a70fa9
Thauan <thauantrindade@outlook.com>
2018-10-14 12:51:27 -0300

    half conclusion 003

--- 003/tagueamento.js
+++ 003/tagueamento.js
@@ -1,9 +1,10 @@
 $('button').on('click', function () {
 
+    var texto = this.innerText.split('-');
 
-    var x = document.createElement("A");
-    var t = document.createTextNode(this.innerText);
-    x.appendChild(t);
-    document.body.appendChild(x);
+    console.log(texto[2]);
 
-});
+    console.log(document.createTextNode(this.innerText));
+
+    //uma vez que todos os botoes foram clicados; criar uma string na ordem que os botoes foram clicados
+}); 
