15b69783d5ebbbd3dfdfb91cad4b1fd557797cf4
Thauan <thauantrindade@outlook.com>
2018-10-14 10:16:51 -0300

    conclusion 001

--- 001/tagueamento.js
+++ 001/tagueamento.js
@@ -1,11 +1,18 @@
-//console.log()
-
-
-if('#botoes'.fixed){
-    console.log(alert("Teste."));
-}
-
-
-function consola(s) {
-    try { console.log(s); } catch (e) { alert(s); }
-}
+$(document).ready(function() {
+    //Cria um array onde ficará armazenado os textos de cada botão
+    let textos = [];
+  
+    //Seleciona todos os botões fazendo um loop
+    $('button').each(function() {
+      //Adiciona ao array o texto dentro do botão
+      textos.push(this.innerText);
+    });
+  
+    //mostra no console o array
+    console.log(textos);
+  });
+  //Seleciona todos os botões e adiciona um ouviente de evento de click em cada
+  $('button').on('click', function() {
+    //Cria um alerta na tela com o texto do botão
+    alert(this.innerText);
+  });
